133a06fcac617f66429836da1f254debfd0568c1
Nikita Marinets <marinets96@gmail.com>
2021-08-02 00:30:53 +0300

    fix2

--- user-data-file-input.tf
+++ user-data-file-input.tf
@@ -6,8 +6,6 @@ resource "aws_instance" "user_data_example_input_file" {
   # Security group assign to instance
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
-  # key name
-  key_name = var.key_name
   user_data = "${file("apache_config.sh")}"
 
   tags = {
