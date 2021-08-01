23e165cafd0f8d71dabf64b06d9b38715da6f758
Nikita Marinets <marinets96@gmail.com>
2021-08-02 00:27:55 +0300

    fix2

--- variables.tf
+++ variables.tf
@@ -1,9 +1,7 @@
 variable "region" {
-  type    = "string"
   default = "eu-central-1"
 }
 variable "ami_id" {
-  type = "map"
   default = {
     us-east-1    = "ami-035b3c7efe6d061d5"
     eu-west-2    = "ami-132b3c7efe6sdfdsfd"
@@ -11,10 +9,8 @@ variable "ami_id" {
   }
 }
 variable "instance_type" {
-  type    = "string"
   default = "t2.micro"
 }
 variable "key_name" {
-  type    = "string"
   default = "ec2-demo"
 }
